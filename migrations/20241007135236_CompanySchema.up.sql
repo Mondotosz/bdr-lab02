a760da69266de3ef3f720ea0045c08a170d5dc14
Mondotosz <git@moncorp.ch>
2024-10-07 16:25:30 +0200

    WIP

--- migrations/20241007135236_CompanySchema.up.sql
+++ migrations/20241007135236_CompanySchema.up.sql
@@ -1,4 +1,37 @@
 -- Add up migration script here
 create schema company;
 
+set search_path to company;
 
+create table employee {
+  ssn char(9) primary key,
+  fname varchar(15) not null,
+  minit char(1),
+  lname varcahr(15) not null,
+  bdate date,
+  address 
+};
+
+create table department {
+
+};
+
+create table dept_location {
+
+};
+
+create table project {
+
+};
+
+create table works_on {
+
+};
+
+create table dependent {
+
+};
+
+create table location {
+
+};
