f007c37f15304c89727cd3c5f724ea1eab7709c5
Mondotosz <git@moncorp.ch>
2024-10-15 09:14:47 +0200

    Ex 1.4.5 finished

--- migrations/20241011172008_Ex1.4.5.up.sql
+++ migrations/20241011172008_Ex1.4.5.up.sql
@@ -6,6 +6,7 @@ BEGIN;
 -- (a)
 -- WARN: Doesn't work since the employee is referenced in `works_on` and violates
 -- the foreign key constraint. Can be avoided by specifying the `ON UPDATE` action
+--
 -- NOTE: the value 7 will be cast to a string here but if it was in the `WHERE`
 -- clause it would result in an error
 -- UPDATE employee
@@ -33,6 +34,7 @@ BEGIN;
 -- (b)
 -- WARN: Doesn't work since it violates the foreign key `fk_essn` on `works_on`.
 -- This can be avoided by specifying the `ON DELETE` action
+--
 -- DELETE FROM employee
 -- WHERE
 --     ssn = '999887777';
@@ -46,5 +48,27 @@ WHERE
 
 ROLLBACK;
 
+-- (c)
+-- WARN: Doesn't work since there is no project with id 5. We either need to
+-- insert a 5th project or not insert the second tuple.
+--
+-- INSERT INTO
+--     works_on
+-- VALUES
+--     ('123456789', 3, 10),
+--     ('123456789', 5, 10);
+-- (d)
+--
+-- WARN: Doesn't work since it violates the foreign key constraint `fk_dno` on
+-- `employee`, `fk_dnum` on `project` and `fk_dnumber` on `dept_locations`. We
+-- could try and overcome this issue by using `ON DELETE CASCADE` for each of
+-- those constraints but that would have rippling effects since deleting a project
+-- also implies deleting every `works_on` relationships that were linked. We might
+-- want to take it all down at once but since it wasn't explicitly asked, let's
+-- not do it.
+--
+-- DELETE FROM department
+-- WHERE
+--     dnumber = 5;
 SET
     SEARCH_PATH TO public;
